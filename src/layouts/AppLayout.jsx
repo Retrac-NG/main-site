8f98448dfe6a6c5f075254d63c55eeb8be59dd8d
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-09-21 20:58:30 +0100

    chore: almost done with bootstrapping

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      {/* ====== Navbar */}
+      {children}
+      {/* ====== footer */}
+    </div>
+  );
+};
+
+export default AppLayout;
