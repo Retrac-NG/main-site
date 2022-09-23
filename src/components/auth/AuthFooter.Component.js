639fdaf9707ee7a7a6637dfe92c919e4d39dc90c
Retrac Nigeria <114071089+Retrac-NG@users.noreply.github.com>
2022-09-23 19:59:27 +0100

    chore: added login page (#10)
    Co-authored-by: Emmanuel Alawode <alawodeemmanuel2@gmail.com>

---
+++ src/components/auth/AuthFooter.Component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+import { footerData } from '../../data/auth.data';
+
+const AuthFooterComponent = () => {
+  return (
+    <footer className='h-14 w-full absolute bottom-3 text-gray-200/90 font-secondary px-8 md:px-10 lg:px-80 flex flex-col gap-4 md:flex-row justify-between items-center text-sm'>
+      <p>All rights reserved. Copyright © 2022 ReTrac.</p>
+
+      <div className='flex gap-5'>
+        {footerData &&
+          footerData.map((item, index) => (
+            <Link href={item.link} passHref key={index}>
+              <a>{item.text}</a>
+            </Link>
+          ))}
+      </div>
+    </footer>
+  );
+};
+
+export default AuthFooterComponent;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import Head from 'next/head';
 import AboutusComponent from '../components/about/Aboutus.Component';
 import HeroComponent from '../components/about/Hero.Component';
 import CommunityComponent from '../components/landing/Community.Component';
-import GetStartedComponent from '../components/landing/GetStarted.Component';
 import AppLayout from '../layouts/AppLayout';
+import GetInTouchComponent from '../components/about/GetInTouch.Component';
 
 const About = () => {
-  return <div className='flex flex-col w-full min-h-screen items-center'>
-    <HeroComponent />
-    <main className='flex h-auto py-10 px-10 rounded-2xl shadow-2xl mb-10 bg-gradient-to-b from-indigo-50 to-white w-[97%] gap-10 items-center flex-col justify-center -mt-10'>
-      <AboutusComponent />
-      <GetStartedComponent />
-      <CommunityComponent />
-
-    </main>
-  </div>;
+  return (
+    <div className='flex flex-col w-full min-h-screen items-center'>
+      <Head>
+        <title>ReTrac NG | About</title>
+        <meta name='description' content='Retrac Main site ' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <HeroComponent />
+      <main className='flex h-auto py-10 px-10 rounded-2xl shadow-2xl mb-10 bg-gradient-to-b from-indigo-50 to-white w-[97%] gap-10 items-center flex-col justify-center -mt-10'>
+        <AboutusComponent />
+        <GetInTouchComponent />
+        <CommunityComponent />
+      </main>
+    </div>
+  );
 };
 
 export default About;
