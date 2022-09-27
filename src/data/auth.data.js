c47243440293693962188bb203ae4573dc1d1089
Retrac Nigeria <114071089+Retrac-NG@users.noreply.github.com>
2022-09-27 22:17:08 +0100

    Development (#14)
    * chore: added login page
    
    * fix: small changes
    
    Co-authored-by: Emmanuel Alawode <alawodeemmanuel2@gmail.com>

--- src/data/auth.data.js
+++ src/data/auth.data.js
@@ -5,11 +5,8 @@ export const footerData = [
   },
   {
     text: 'About us',
-<<<<<<< HEAD
-    link: 'about',
-=======
     link: '/about',
->>>>>>> 2b2b44a5e3520d29f707ce79d72011616cf9c7bf
+    link: 'about',
   },
   {
     text: 'Blog',
