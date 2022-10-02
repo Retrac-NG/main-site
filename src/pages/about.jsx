62505e576da05e69fbb16f125cf6dbbe9ba9dcb6
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2022-10-02 10:53:40 +0100

    Oddfeeling feat management (#20)
    * chore: added login page
    
    * fix: small changes
    
    * chore: completed initial user profile page
    
    * chore: added 1 more section to profile page
    
    * fix: fixed merge issues again
    
    * feat: finished the management page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,14 +8,14 @@ import GetInTouchComponent from '../components/about/GetInTouch.Component';
 
 const About = () => {
   return (
-    <div className='flex flex-col w-full min-h-screen items-center'>
+    <div className='flex flex-col w-full min-h-screen bg-gray-100 items-center'>
       <Head>
         <title>ReTrac NG | About</title>
         <meta name='description' content='Retrac Main site ' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <HeroComponent />
-      <main className='flex h-auto py-10 px-10 rounded-2xl shadow-2xl mb-10 bg-gradient-to-b from-indigo-50 to-white w-[97%] gap-10 items-center flex-col justify-center -mt-10'>
+      <main className='main'>
         <AboutusComponent />
         <GetInTouchComponent />
         <CommunityComponent />
