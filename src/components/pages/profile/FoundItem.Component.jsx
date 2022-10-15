fd11732ff2714a7218d631ca31236a2b5238d40e
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-10-15 21:45:28 +0100

    chore: restructure started

--- src/components/pages/profile/FoundItem.Component.jsx
+++ src/components/pages/profile/FoundItem.Component.jsx
@@ -1,7 +1,5 @@
-
 import React, { useState } from 'react';
 import RecentComponent from './Recent.component';
-import ProfileItemCardComponent from '../lib/cards/ProfileItemCard.component';
 import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
